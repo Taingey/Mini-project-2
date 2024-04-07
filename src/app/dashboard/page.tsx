cda4507cda83e0a6432dcf2b386226de9b7f6e70
unknown <taingey2966@gmail.com>
2024-04-07 14:08:37 +0700

    Mini Project Update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,11 +5,14 @@ const Dashboard: React.FC = () => {
   return (
     <div className="w-full overflow-hidden">
       <h1 className="font-bold text-center mt-6 mb-6 text-[25px]">User Data</h1>
+
       <UserTables
         id={1}
         name="taingey"
         email="taingey@gmail.com"
         image="cat.jpg"
+        price={0}
+        desc= "null"
       />
     </div>
   );
